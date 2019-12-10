3c846fe17277e5cbb3cf4dbbfc95bf49337f165e
hilmawanyr <hilmawanyr@gmail.com>
2019-12-10 11:08:33 +0700

    half complete form assesment

--- application/models/Assessment_model.php
+++ application/models/Assessment_model.php
@@ -50,8 +50,118 @@ class Assessment_model extends CI_Model {
 		$this->db->order_by('a.grade, b.name', 'asc');
 		return $this->db->get()->result();
 	}
-			
+	
+	/**
+	 * Get competency dictionary for each job title
+	 * @param int $jobtitle
+	 * @return array
+	 */
+	public function get_competency(int $jobtitle) : array
+	{
+		$this->db->select('
+			a.job_id,
+			a.skill_id,
+			a.level,
+			b.name_id,
+			b.description');
+		$this->db->from('skill_matrix a');
+		$this->db->join('skill_dictionaries b', 'a.skill_id = b.id');
+		$this->db->where('a.job_id', $jobtitle);
+		$this->db->where('a.deleted_at');
+		return $this->db->get();
+	}
 
+	/**
+	 * Check whether assessment form is exist
+	 * @param string $activeYear
+	 * @param int $jobtitle
+	 * @return array
+	 */
+	public function is_assessment_form_exist(string $activeYear, int $jobtitle) : array
+	{
+		$this->db->where('job_id', $jobtitle);
+		$this->db->like('code', $activeYear, 'before');
+		return $this->db->get('assessment_forms');
+	}
+
+	/**
+	 * Get competency for each employes by their job title
+	 * @param string $activeYear
+	 * @param int $jobtitle
+	 * @return array
+	 */
+	public function competency_by_jobtitle(string $activeYear, int $jobtitle) : array
+	{
+		$this->db->select('*');
+		$this->db->from('employes a');
+		$this->db->join('assessment_forms b', 'a.nik = b.nik', 'left');
+		$this->db->where('a.job_title_id', $jobtitle);
+		$this->db->like('b.code', $activeYear, 'before');
+		$this->db->order_by('b.total_poin ASC, a.name ASC');
+		return $this->db->get();
+	}
+
+	/**
+	 * Get number of filled assessment
+	 * @param int $jobtitle
+	 * @return int
+	 */
+	public function complete_assessment(int $jobtitle) : int
+	{
+		$this->db->select('a.code');
+		$this->db->from('assessment_forms a');
+		$this->db->join('assessment_form_questions b', 'a.id = b.form_id');
+		$this->db->where('b.poin IS NOT NULL', NULL, FALSE);
+		$this->db->where('a.job_id', $jobtitle);
+		return $this->db->get()->num_rows();
+	}
+
+	/**
+	 * Check is employe has assessment form
+	 * @param int $jobtitle
+	 * @param string $activeYear
+	 * @param string $nik
+	 * @return int
+	 */
+	public function _is_employe_has_form(int $jobtitle, string $activeYear, string $nik) : int
+	{
+		// assessment form code
+		$code = 'AF-'.$employe->job_title_id.'-'.$activeYear;
+		$this->db->where('code', $code);
+		$this->db->where('nik', $nik);
+		$this->db->where('job_id', $jobtitle);
+		return $this->db->get('assessment_forms')->num_rows();
+	}
+	
+	/**
+	 * Create question competency
+	 * @param int $jobtitle
+	 * @return array
+	 */
+	public function create_assessment_question(int $jobtitle) : array
+	{
+		$this->db->select('
+				job_titles.id as job_title,
+                job_titles.group,
+                skill_units.level AS lv_unit,
+                skill_matrix.level AS lv_matrix,
+                skill_matrix.skill_id,
+                skill_units.id as unit_id,
+                skill_units.description,
+                (SELECT weight FROM assessment_form_weight JOIN job_groups 
+                ON job_groups.code = assessment_form_weight.job_group_code 
+                WHERE job_groups.id = job_titles.group 
+                AND assessment_form_weight.level = skill_matrix.level 
+                AND assessment_form_weight.unit = skill_units.level) as bobot'
+			);
+		$this->db->from('job_titles');
+		$this->db->join('skill_matrix', 'skill_matrix.job_id = job_titles.id');
+		$this->db->join('skill_units', 'skill_units.id_dictionary = skill_matrix.skill_id');
+		$this->db->where('skill_units.deleted_at');
+		$this->db->where('skill_matrix.deleted_at');
+		$this->db->where('job_titles.id', $jobtitle);
+		return $this->db->get();
+	}
 }
 
 /* End of file Assessment.php */
--- application/modules/assessment/controllers/Assessment.php
+++ application/modules/assessment/controllers/Assessment.php
@@ -47,8 +47,108 @@ class Assessment extends CI_Controller {
 	public function form(int $jobtitle) : void
 	{
 		$data['active_year'] = get_active_year();
-		
+
+		$get_employes = $this->db->where('job_title_id', $jobtitle)->get('employes');
+		$data['jobTitleName'] = $this->db->where('id', $jobtitle)->get('job_titles');
+
+		// load competency
+		$data['dictionary'] = $this->assessment->get_competency($jobtitle);
+
+		// check whether assessment formhas generate or not
+		$isFormExist = $this->assessment->is_assessment_form_exist($data['active_year'], $jobtitle);
+
+		// generate form if job title has competency
+		if ($data['dictionary']->num_rows() > 0) {
+			// insert form assessment if those doesn't exist
+			if ($isFormExist->num_rows() < 1) {
+				$this->_generate_form_assessment($get_employes, $jobtitle);
+			}
+
+			// change value of $get_employe if job_titles has competency matrixes
+			$get_employes = $this->assessment->competency_by_jobtitle($data['active_year'], $jobtitle);
+		}
+
+		// check whether form has submited or not
+		$data['isSubmited'] = $this->db->like('code', 'AF-'.$jobtitle.'-'.$data['active_year'], 'BOTH')
+										->get('assessment_validations')
+										->num_rows();
+
+		/** then check number of assessment per job title
+         * and compare with number of complete assessment
+         * to get the comparison which will be use
+         * to check whether the form can be submit or not
+         */
+		// number of statement per job title
+		$data['statementAmount'] = $isFormExist->num_rows() * $data['dictionary']->num_rows();
+
+		// number of filled assessment
+		$data['completeAssessment'] = $this->assessment->complete_assessment($jobtitle);
+		$data['employes'] = $get_employes;
+		$data['page'] = 'assessment_form_v';
+		$this->load->view('template/template', $data);
 	}
+
+	/**
+	 * Create form assessment content
+	 * @param array $employes
+	 * @param int $jobtitle
+	 * @return void
+	 */
+	private function _generate_form_assessment(array $employes, int $jobtitle) : void
+	{
+		$activeYear = get_active_year();
+
+		foreach ($employes as $employe) {
+			$isEmployeHasForm = $this->assessment->is_employe_has_form(
+									$employe->job_title_id,
+									$activeYear,
+									$employe->nik
+								);
+			if ($isEmployeHasForm < 1) {
+				// create an array of assessment form data to make insert batch
+				$assessmentForm[] = [
+					'code' => 'AF-'.$employe->job_title_id.'-'.$activeYear,
+					'nik' => $employe->nik,
+					'job_id' => $employe->job_title_id
+				];
+			}
+		}
+		$this->db->insert_batch('assessment_forms', $assessmentForm);
+
+		// insert assesment question
+		$this->_insert_assessment_question($activeYear, $jobtitle);
+	}
+
+	/**
+	 * Insert assessment question
+	 * @param string $activeYear
+	 * @param int $jobtitle
+	 * @return null
+	 */
+	private function _insert_assessment_question(string $activeYear, int $jobtitle)
+	{
+		$getForm = $this->assessment->is_assessment_form_exist($activeYear, $jobtitle);
+		foreach ($getForm as $form) {
+			// check is assessment question form has exist
+			$isQuestionExist = $this->db->where('form_id', $form->id)->get('assessment_form_question');
+
+			// if question doesn't exist, create it
+			if ($isQuestionExist->num_rows() < 1) {
+				$questionCompetency = $this->assessmentForm->create_assessment_question($form->job_id);
+
+				foreach ($questionCompetency->result() as $competency) {
+					$assessmentQuestion[] = [
+						'form_id' => $form->id,
+						'skill_unit_id' => $competency->unit_id,
+						'weight' =>$competency->bobot
+					];
+				}
+			}
+		}
+		$this->db->insert_batch('assessment_form_questions', $assessmentQuestion);
+		return;
+	}
+	
 
 }
 
